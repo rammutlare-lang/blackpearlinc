687e42d753b77aaa1780bc42bb385aa0642fb86f
Black Pearl Inc <hello@blackpearlinc.co.za>
2026-08-12 02:21:51 +0200

    Recrop hero photos to keep people/activity in frame and lighten the dark overlay

--- components/HeroBackground.tsx
+++ components/HeroBackground.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 export function HeroBackground({ src, alt = "" }: { src: string; alt?: string }) {
   return (
     <div className="absolute inset-0">
-      <Image src={src} alt={alt} fill priority className="object-cover opacity-45" sizes="100vw" />
-      <div className="absolute inset-0 bg-gradient-to-b from-tw-black/85 via-tw-black/80 to-tw-black" />
+      <Image src={src} alt={alt} fill priority className="object-cover opacity-75" sizes="100vw" />
+      <div className="absolute inset-0 bg-tw-black/55" />
+      <div className="absolute inset-0 bg-gradient-to-t from-tw-black/70 via-transparent to-tw-black/40" />
     </div>
   );
 }
